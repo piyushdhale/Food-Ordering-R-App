9c02a477b5d837b507305b6034a758a3c90d2a95
piyushdhale <piyushdhale777@gmail.com>
2024-08-30 16:13:07 +0530

    dynamic Swiggy api added useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,23 @@
 import ReasturantCard from "./ResturantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
 
-  const [listOfRes,setListOfRes]=useState(resList);
+  const [listOfRes,setListOfRes]=useState([]);
   
+
+  useEffect(()=> {
+    fetchData();
+  } , [])
+
+  const fetchData = async () =>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    
+    const json = await data.json();
+    console.log(json)
+    setListOfRes(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  };
+
     return(
       <div className='body'>
         
