8fac152337902935305f04cdfdffce1d556fd90a
piyushdhale <piyushdhale777@gmail.com>
2024-09-03 21:09:16 +0530

    Code formated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
-import Header from './components/Header';
-import Body from './components/Body';
+import "./App.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
 
-
-
-const AppLayout = () =>{
+const AppLayout = () => {
   return (
-    <div className='app'>
-      <Header/>
+    <div className="app">
+      <Header />
       <marquee width="100%" height="25px" direction="right">
-        WebSite under developement</marquee>
-      <Body/>
+        WebSite under developement
+      </marquee>
+      <Body />
     </div>
-  )
-}
+  );
+};
 
 export default AppLayout;
--- src/components/Body.js
+++ src/components/Body.js
@@ -15,62 +15,81 @@ const Body = () => {
     fetchData();
   } , [])
 
-  const fetchData = async () =>{
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-    
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
     const json = await data.json();
-    console.log(json)
-    setListOfRes(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    setFilterRes(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    console.log(json);
+    setListOfRes(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilterRes(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
-    return listOfRes.length===0 ?
-     <Shimmer/> : (
-      <div className='body'>
-        
-        <div className='filter'>
-          
-          <div className="search">
-            <input 
-              type="text" 
-              placeholder="search Resturant" 
-              value ={searchtext} 
-              className="search-box" 
-              onChange={(e) => {
-                setSearchtext(e.target.value);
-            }}/>
+  return listOfRes.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="search Resturant"
+            value={searchtext}
+            className="search-box"
+            onChange={(e) => {
+              setSearchtext(e.target.value);
+            }}
+          />
 
-            <button className="searchBtn" 
-              onClick={()=>{
+          <button
+            className="searchBtn"
+            onClick={() => {
               console.log(searchtext);
-              const filteredrestro = listOfRes.filter((restaurant)=>restaurant.info.name.toLowerCase().includes(searchtext.toLowerCase()))
+              const filteredrestro = listOfRes.filter((restaurant) =>
+                restaurant.info.name
+                  .toLowerCase()
+                  .includes(searchtext.toLowerCase())
+              );
               setFilterRes(filteredrestro);
-            }}>search</button>
+            }}
+          >
+            search
+          </button>
+        </div>
 
-          </div>
-          
-          <button 
-            className="filter-btn" 
-            onClick={()=>{
-                const fList = listOfRes.filter((res)=>res.info.avgRating > 4);
-                setFilterRes(fList)
-            }}>Top Rated Resturants</button>
-            
-            <button 
-              className="filter-btn" 
-              onClick={()=>{setFilterRes(listOfRes);
-                setSearchtext("");}}>
-            Show All Restaurants
-            </button>
-        </div>
-  
-        <div className='res-container'>
-          {
-            filterRes.map(e =><ResturantCard key={e.info.id} resObj={e}/>)
-          }
-        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const fList = listOfRes.filter((res) => res.info.avgRating > 4);
+            setFilterRes(fList);
+          }}
+        >
+          Top Rated Resturants
+        </button>
+
+        <button
+          className="filter-btn"
+          onClick={() => {
+            setFilterRes(listOfRes);
+            setSearchtext("");
+          }}
+        >
+          Show All Restaurants
+        </button>
       </div>
-    )
+
+      <div className="res-container">
+        {filterRes.map((e) => (
+          <ResturantCard key={e.info.id} resObj={e} />
+        ))}
+      </div>
+    </div>
+  );
   }
 
   export default Body;
